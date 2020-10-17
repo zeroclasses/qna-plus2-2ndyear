6e268ce1270eb0fe9ea72836d4874eb193cae13c
zeroclasses <zeroclasses.odisha@gmail.com>
2020-10-17 09:51:55 +0000

    Added serial numbers

--- lib.js
+++ lib.js
@@ -2,10 +2,12 @@
 
  function renderQnA(qna) {
   var template = document.getElementById('qaTemplate').innerHTML;
+  let counter = 0;
   for(let qa of qna){
-  var rendered = Mustache.render(template, qa);
-  console.log(qa,rendered);
-  document.getElementById('qna').innerHTML += rendered;
+    counter++;
+    qa.counter=counter;
+    var rendered = Mustache.render(template, qa);
+    document.getElementById('qna').innerHTML += rendered;
   }
 
   
@@ -16,7 +18,6 @@ function renderTable(data){
      var template = document.getElementById('questionsTrTemplate').innerHTML;
         for(let row of data){
             var rendered = Mustache.render(template, row);
-            console.log("details",row,rendered);
             document.getElementById('chaptersTable').innerHTML += rendered;
         }
 }
